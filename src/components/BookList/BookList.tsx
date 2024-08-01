82ea61231268391798d51e9167256ed9825ba240
Rohith <tech247gb@gmail.com>
2024-08-01 11:24:17 +0530

    hide pagination in book list if no result

--- src/components/BookList/BookList.tsx
+++ src/components/BookList/BookList.tsx
@@ -79,7 +79,7 @@ const BookList: React.FC = () => {
 
     return (
         <>
-            <div className="container mx-auto px-4 mb-20 mt-10">
+            <div className="container mx-auto px-4 mb-20 mt-10 min-h-screen">
                 <h2 className="text-4xl font-bold mb-6 text-center text-primary">Reviews</h2>
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 text-center">
                     {
@@ -88,15 +88,15 @@ const BookList: React.FC = () => {
                                 {books.length > 0 ? (
                                     books.map((book, index) => (
                                         <Link to={`/reviews/${book.id}`} >
-                                        <div key={index} className="bg-white rounded-lg shadow-lg overflow-hidden transition-transform transform hover:scale-105">
-                                            <img src={book.coverImage} alt={book.title} className="w-full h-48 object-cover" />
-                                            <div className="p-6">
-                                                <h3 className="text-2xl font-semibold mb-2 text-gray-800">{book.title}</h3>
-                                                <p className="text-gray-600 mb-1"><strong>Author:</strong> {book.author}</p>
-                                                <StarRating rating={book.rating} />
-                                                <p className="text-gray-800 mt-2 mb-6 whitespace-pre-line">{book.review}</p>
+                                            <div key={index} className="bg-white rounded-lg shadow-lg overflow-hidden transition-transform transform hover:scale-105">
+                                                <img src={book.coverImage} alt={book.title} className="w-full h-48 object-cover" />
+                                                <div className="p-6">
+                                                    <h3 className="text-2xl font-semibold mb-2 text-gray-800">{book.title}</h3>
+                                                    <p className="text-gray-600 mb-1"><strong>Author:</strong> {book.author}</p>
+                                                    <StarRating rating={book.rating} />
+                                                    <p className="text-gray-800 mt-2 mb-6 whitespace-pre-line">{book.review}</p>
+                                                </div>
                                             </div>
-                                        </div>
                                         </Link>
                                     ))
                                 ) : (
